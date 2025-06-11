ad569ecfe5f0f92f99efefd6309fe79d01b3934d
ShrutiKrishna1379 <shrutikrishna.rsvm@gmail.com>
2025-06-11 12:11:44 +0530

    Add Codes

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,5 +1,5 @@
 import express from "express"
-import {editProfile, getCurrentUser, getOtherUsers} from "../controllers/user.controllers.js"
+import {editProfile, getCurrentUser, getOtherUsers, search} from "../controllers/user.controllers.js"
 import isAuth from "../middlewares/isAuth.js"
 import { upload } from "../middlewares/multer.js"
 
@@ -7,5 +7,6 @@ const userRouter=express.Router()
 userRouter.get("/current",isAuth, getCurrentUser)
 userRouter.get("/others",isAuth, getOtherUsers)
 userRouter.put("/profile",isAuth,upload.single("image"), editProfile)
+userRouter.get("/search",isAuth, search)
 
 export default userRouter
--- frontend/src/components/SideBar.jsx
+++ frontend/src/components/SideBar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import dp from "../assets/dp.png"
 import { IoIosSearch } from "react-icons/io";
 import { BiLogOutCircle } from "react-icons/bi";
@@ -7,13 +7,14 @@ import { useDispatch, useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 import axios from 'axios';
 import { serverUrl } from '../main';
-import { setOtherUsers, setSelectedUser, setUserData } from '../redux/userSlice';
+import { setOtherUsers, setSearchData, setSelectedUser, setUserData } from '../redux/userSlice';
 
 
 function SideBar(){
-    let {userData,otherUsers,selectedUser,onlineUsers}=useSelector(state => state.user)
+    let {userData,otherUsers,selectedUser,onlineUsers,searchData}=useSelector(state => state.user)
     let navigate=useNavigate()
-    let [search,setSearch]=useState(false)
+    let [search,setSearch]=useState("")
+    let [input,setInput]=useState("")
     let dispatch=useDispatch()
     const handleLogOut=async ()=>{
         try{
@@ -26,13 +27,50 @@ function SideBar(){
             console.log(error)
         }
     }
+    const handlesearch=async ()=>{
+        try{
+            let result=await axios.get(`${serverUrl}/api/user/search?query=${input}`,{withCredentials:true})
+            dispatch(setSearchData(result.data))
+            //console.log("search result:", result.data)
+        }
+        catch(error){
+            console.log(error)
+        }
+    }
+    useEffect(()=>{
+        if(input){
+            handlesearch()
+        }
+    },[input])
 
   return(
-    <div className={`lg:w-[30%] w-full h-full overflow-hidden lg:block bg-slate-200 ${!selectedUser?"block":"hidden"}`}>
+    <div className={`lg:w-[30%] w-full h-full overflow-hidden lg:block bg-slate-200 relative ${!selectedUser?"block":"hidden"}`}>
+        
+        {/* Log out button */}
         <div className='w-[50px] h-[50px] mt-[10px] rounded-full overflow-hidden flex justify-center items-center bg-[#e4cb0ac8] shadow-gray-500 text-gray-700 shadow-lg cursor-pointer fixed bottom-[20px] left-[25px]' onClick={handleLogOut}>
             <BiLogOutCircle  className='w-[20px] h-[20px]'/>
         </div>
-
+        
+        {/*Search bar */}
+        {input.length>0&& 
+            <div className='flex bg-[white] w-full h-full overflow-y-auto items-center flex-col gap-[10px] absolute top-[230px] z-[150] pt-[20px] shadow-lg' >
+                {searchData?.map((user)=>(
+                    
+                        <div className='w-[95%] h-[70px] flex items-center gap-[20px]  hover:bg-[#f2dc7d] border-b-2 border-gray-400  cursor-pointer' onClick={()=>{ dispatch(setSelectedUser(user)), setInput(""), setSearch(false)} }>
+                            <div className='relative flex justify-center items-center'>
+                                <div className='w-[60px] h-[60px] rounded-full overflow-hidden flex justify-center items-center ' >
+                                    <img src={user.image || dp} alt="" className='h-[100%]' />
+                                </div>
+                                {onlineUsers?.includes(user._id) && <span className='w-[12px] h-[12px]  absolute bottom-[6px] right-[-1px] bg-[#3aff20] shadow-gray-500 shadow-md'></span>}
+                            </div>
+                            <h1 className='text-gray-800 font-semibold text-[20px]'>{user.name || user.userName}</h1>
+                        </div> 
+                    
+                ))}
+            </div>
+        }
+    
+        {/* Main sidebar */}
         <div className='w-full h-[300px] bg-[#e4cb0ac8] rounded-b-[30%] shadow-gray-400 shadow-lg flex flex-col justify-center px-[20px]'>
             <h1 className='text-white font-bold text-[25px]'>LinkUp</h1>
             <div className='w-full flex justify-between items-center'>
@@ -52,7 +90,7 @@ function SideBar(){
                 {search && 
                     <form className='w-[90%] h-[50px]  bg-white shadow-gray-500 shadow-lg flex items-center gap-[10px] mt-[10px] rounded-full overflow-hidden px-[20px]'>
                         <IoIosSearch  className='w-[20px] h-[20px]'/>
-                        <input type="text" placeholder='Search users...' className='w-full h-full p-[10px] text-[17px] outline-0 border-0' />
+                        <input type="text" placeholder='Search users...' className='w-full h-full p-[10px] text-[17px] outline-none border-0' onChange={(e)=>setInput(e.target.value)} value={input} />
                         <RxCross2 className='w-[20px] h-[20px] cursor-pointer' onClick={()=>setSearch(false)} />
                     </form>
                 }
@@ -72,6 +110,7 @@ function SideBar(){
 
         </div>
 
+        {/* Other users list */}
         <div className='w-full h-[50%] mt-[20px] items-center overflow-auto flex flex-col gap-[20px]'>
             {otherUsers?.map((user)=>(
                 <div className='w-[95%] h-[60px] flex items-center gap-[20px] shadow-gray-500 bg-white shadow-lg rounded-full hover:bg-[#f2dc7d] cursor-pointer' onClick={()=>dispatch(setSelectedUser(user))}>
