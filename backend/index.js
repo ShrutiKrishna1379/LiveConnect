539ef5cf3b095aa12f285316ceefe1bd1ffac92b
ShrutiKrishna1379 <146547542+ShrutiKrishna1379@users.noreply.github.com>
2025-08-25 10:09:25 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ const port = process.env.PORT || 5000
 
 
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://liveconnect-f8m8.onrender.com",
     credentials:true
 }))
 app.use(express.json())
