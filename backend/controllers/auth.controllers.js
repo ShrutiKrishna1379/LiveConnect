a83eb12ec30a62ae89515ff6e649897c1325df8c
ShrutiKrishna1379 <146547542+ShrutiKrishna1379@users.noreply.github.com>
2025-08-25 10:11:42 +0530

    Update auth.controllers.js

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -26,8 +26,8 @@ export const signUp = async (req, res) => {
         const token=await genToken(user._id)
         res.cookie("token",token,{
             httpOnly:true,
-            secure:false,
-            sameSite:"Strict",
+            secure:true,
+            sameSite:"None",
             maxAge:7*24*60*60*1000
         })
 
@@ -54,8 +54,8 @@ export const login = async (req, res) => {
         const token=await genToken(user._id)
         res.cookie("token",token,{
             httpOnly:true,
-            secure:false,
-            sameSite:"Strict",
+            secure:true,
+            sameSite:"None",
             maxAge:7*24*60*60*1000
         })
 
